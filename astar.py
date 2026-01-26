a17799a93b05a70ab754650fdb188ea1be83a5e3
DemirDorukDilek <43827034+DemirDorukDilek@users.noreply.github.com>
2026-01-26 09:24:04 +0300

    side 2-1

--- astar.py
+++ astar.py
@@ -4,28 +4,37 @@ import heapq
 from itertools import product
 np.genfromtxt("./mapcik.txt")
 
-net = nx.read_edgelist("./mapcik.txt", edgetype=int)
+net = nx.read_edgelist("./mapcik.txt", edgetype=int, nodetype=int)
 
+class Anode:
+    def __init__(self,state,cost=None,parent=None):
+        self.state=state
+        self.cost=cost
+        self.parent=parent
 
-def astar(start=(1,3),goal=(12,10)):
+def astar(net,start=(1,3),goal=(12,10),heuristic=None):
     OPEN = []
 
-    heapq.heappush(OPEN,(0,start,None))
+    heapq.heappush(OPEN,Anode(start,0,None))
 
     CLOSED = []
 
     while OPEN:
         q = heapq.heappop(OPEN)
-
-        for s in product(*([u, *G.adj[u]] for u in q[1])):
+        heapq.heappush(CLOSED,q)
+        for s in product(*([u, *net.adj[u]] for u in q.state)):
             if s == goal:
-                pass
-                # TODO return
-            g = q[0]+1
-            h = None # TODO h
+                ptr = q
+                path = [goal]
+                while ptr:
+                    path.append(ptr.state)
+                    ptr = ptr.parent
+                return path.reverse()
+            g = q.cost+1
+            h = heuristic(s,goal)
             f = g+h
             # TODO make skips
-            heapq.heappush(OPEN,(f,s,q)) # TODO check correctensy
-        
-        heapq.heappush(CLOSED,q)
-            
+            heapq.heappush(OPEN,Anode(s,f,q)) # TODO check correctensy
+  
+astar(net)
+# TODO make SIC
